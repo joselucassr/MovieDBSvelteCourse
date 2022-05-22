43d6e11b1272fdbbc531d693fbeb6aa23a51a99a
Lucas <joselucasrs@gmail.com>
2022-05-22 20:04:24 -0300

    Updating Movie type.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,6 @@
 export interface Movie {
 	adult: boolean;
 	backdrop_path: string;
-	genre_ids: number[];
 	id: number;
 	original_language: string;
 	original_title: string;
@@ -13,4 +12,23 @@ export interface Movie {
 	video: boolean;
 	vote_average: number;
 	vote_count: number;
+	// Optional
+	belongs_to_collection?: unknown;
+	budget?: number;
+	genres?: { id: number; name: string };
+	genre_ids?: number[];
+	homepage?: string | null;
+	imdb_id?: string | null;
+	production_companies?: {
+		name: string;
+		id: number;
+		logo_path: string | null;
+		origin_country: string;
+	}[];
+	production_countries?: { iso_3166_1: string; name: string }[];
+	revenue?: number;
+	runtime?: number | null;
+	spoken_languages?: { iso_639_1: string; name: string }[];
+	status?: 'Rumored' | 'Planned' | 'In Production' | 'Post Production' | 'Released' | 'Canceled';
+	tagline?: string | null;
 }
